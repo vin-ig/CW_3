7a83f356bed9096cf173c102dac629ef8ee710ed
Harry <vin-ig@yandex.ru>
2022-06-15 15:32:26 +0300

    Favourites done

--- dao/favourites.py
+++ dao/favourites.py
@@ -1,20 +1,10 @@
-from sqlalchemy import desc
-
 from dao.model.favourites import Favourites
-from dao.model.director import Director
-from dao.model.genre import Genre
-from constants import QUERY
 from dao.model.movie import Movie
-from utils import get_pagination
 
 
 class FavouritesDAO:
 	def __init__(self, session):
 		self.session = session
-
-	def get_one(self, uid):
-		"""Возвращает один фильм"""
-		return self.session.query(Favourites).get(uid)
 
 	def get_all(self, uid):
 		"""Возвращает избранные фильмы"""
@@ -23,10 +13,12 @@ class FavouritesDAO:
 			all()
 
 	def add(self, data):
+		"""Добавляет фильм в избранное"""
 		self.session.add(Favourites(**data))
 		self.session.commit()
 
 	def delete(self, movie_id):
+		"""Удаляет фильм из избранного"""
 		raw = self.session.query(Favourites).filter(Favourites.movie_id == movie_id).one()
 		self.session.delete(raw)
 		self.session.commit()
